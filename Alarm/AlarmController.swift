e146094a4f0ba1f0dc162d846b5054d0fd60e072
reich452 <reich452@regis.edu>
2017-06-05 16:19:46 -0600

    Added new AlarmController file

---
+++ Alarm/AlarmController.swift
@@ -0,0 +1,13 @@
+//
+//  AlarmController.swift
+//  Alarm
+//
+//  Created by Nick Reichard on 6/5/17.
+//  Copyright © 2017 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class AlarmController {
+    
+}
